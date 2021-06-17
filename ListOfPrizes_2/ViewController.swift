f92527c8824ffc67c6a86a811130b3406c850dec
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-17 18:57:03 +0300

    Initial Commit

---
+++ ListOfPrizes_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListOfPrizes_2
+//
+//  Created by Sergey Lukaschuk on 17.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
