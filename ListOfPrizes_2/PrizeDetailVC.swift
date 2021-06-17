0c9b447588982887a23d0d12105991d12ae5d3ab
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-17 22:55:59 +0300

    Add 'PrizeCell'

--- ListOfPrizes_2/PrizeDetailVC.swift
+++ ListOfPrizes_2/PrizeDetailVC.swift
@@ -3,11 +3,11 @@
 //  ListOfPrizes_2
 //
 //  Created by Sergey Lukaschuk on 17.06.2021.
-//
+// Detail
 
 import UIKit
 
-class ViewController: UIViewController {
+class PrizeDetailVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
